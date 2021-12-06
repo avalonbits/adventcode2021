56dba17273cba5aae42359cd9f895d04f0710997
Igor Cananea <icc@avalonbits.com>
2021-12-05 22:25:22 -0500

    Refectored walk functions.

--- prob3/part2.go
+++ prob3/part2.go
@@ -35,55 +35,54 @@ func main() {
 	})
 
 	most := []byte{}
-	least := []byte{}
-
 	trie.Walk(func(nodes []collections.TNode[byte]) collections.TNode[byte] {
-		if len(nodes) == 1 {
-			most = append(most, nodes[0].V)
-			return nodes[0]
-		}
-		var zero, one *collections.TNode[byte]
-		for i := range nodes {
-			if nodes[i].V == '0' {
-				zero = &nodes[i]
-			} else if nodes[i].V == '1' {
-				one = &nodes[i]
-			}
+		var zero, one collections.TNode[byte]
+		if ret := common(nodes, &zero, &one); ret != nil {
+			most = append(most, ret.V)
+			return *ret
 		}
 
 		if one.Count >= zero.Count {
-			most = append(most, '1')
-			return *one
+			most = append(most, one.V)
+			return one
 		} else {
-			most = append(most, '0')
-			return *zero
+			most = append(most, zero.V)
+			return zero
 		}
 	})
 
+	least := []byte{}
 	trie.Walk(func(nodes []collections.TNode[byte]) collections.TNode[byte] {
-		if len(nodes) == 1 {
-			least = append(least, nodes[0].V)
-			return nodes[0]
-		}
-		var zero, one *collections.TNode[byte]
-		for i := range nodes {
-			if nodes[i].V == '0' {
-				zero = &nodes[i]
-			} else if nodes[i].V == '1' {
-				one = &nodes[i]
-			}
+		var zero, one collections.TNode[byte]
+		if ret := common(nodes, &zero, &one); ret != nil {
+			least = append(least, ret.V)
+			return *ret
 		}
 
 		if zero.Count <= one.Count {
 			least = append(least, '0')
-			return *zero
+			return zero
 		} else {
 			least = append(least, '1')
-			return *one
+			return one
 		}
 	})
 
 	m, _ := strconv.ParseInt(string(most), 2, 64)
 	l, _ := strconv.ParseInt(string(least), 2, 64)
 	fmt.Println(string(most), m, string(least), l, m*l)
+}
+
+func common(nodes []collections.TNode[byte], zero, one *collections.TNode[byte]) *collections.TNode[byte] {
+	if len(nodes) == 1 {
+		return &nodes[0]
+	}
+	for i := range nodes {
+		if nodes[i].V == '0' {
+			*zero = nodes[i]
+		} else if nodes[i].V == '1' {
+			*one = nodes[i]
+		}
+	}
+	return nil
 }
